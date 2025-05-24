1c77dd6bfc16c8da2ca4de170ce1722b5866da2d
Santhosh Thottingal <santhosh.thottingal@gmail.com>
2025-05-24 15:26:25 +0530

    Re-initilize using tree-sitter
    Reinitialize using tree-sitter latest version. Previous version was not
    compiling - at least the rust bindings were non functional

--- bindings/rust/lib.rs
+++ bindings/rust/lib.rs
@@ -6,7 +6,7 @@
 //! ```
 //! let code = "";
 //! let mut parser = tree_sitter::Parser::new();
-//! parser.set_language(tree_sitter_YOUR_LANGUAGE_NAME::language()).expect("Error loading YOUR_LANGUAGE_NAME grammar");
+//! parser.set_language(tree_sitter_wikitext::LANGUAGE.into()).expect("Error loading wikitext grammar");
 //! let tree = parser.parse(code, None).unwrap();
 //! ```
 //!
@@ -15,18 +15,15 @@
 //! [Parser]: https://docs.rs/tree-sitter/*/tree_sitter/struct.Parser.html
 //! [tree-sitter]: https://tree-sitter.github.io/
 
-use tree_sitter::Language;
+use tree_sitter_language::LanguageFn;
 
-extern "C" {
-    fn tree_sitter_YOUR_LANGUAGE_NAME() -> Language;
+unsafe extern "C" {
+    fn tree_sitter_wikitext() -> *const ();
 }
-
-/// Get the tree-sitter [Language][] for this grammar.
+/// The tree-sitter [`LanguageFn`][LanguageFn] for this grammar.
 ///
-/// [Language]: https://docs.rs/tree-sitter/*/tree_sitter/struct.Language.html
-pub fn language() -> Language {
-    unsafe { tree_sitter_YOUR_LANGUAGE_NAME() }
-}
+/// [LanguageFn]: https://docs.rs/tree-sitter-language/*/tree_sitter_language/struct.LanguageFn.html
+pub const LANGUAGE: LanguageFn = unsafe { LanguageFn::from_raw(tree_sitter_wikitext) };
 
 /// The content of the [`node-types.json`][] file for this grammar.
 ///
@@ -42,11 +39,14 @@ pub const NODE_TYPES: &'static str = include_str!("../../src/node-types.json");
 
 #[cfg(test)]
 mod tests {
+    use crate::tree_sitter_wikitext;
+
     #[test]
     fn test_can_load_grammar() {
         let mut parser = tree_sitter::Parser::new();
+        let language = super::LANGUAGE;
         parser
-            .set_language(super::language())
-            .expect("Error loading YOUR_LANGUAGE_NAME language");
+            .set_language(&language.into())
+            .expect("Error loading wikitext language");
     }
 }
--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ class BdistWheel(bdist_wheel):
     def get_tag(self):
         python, abi, platform = super().get_tag()
         if python.startswith("cp"):
-            python, abi = "cp38", "abi3"
+            python, abi = "cp39", "abi3"
         return python, abi, platform
 
 
@@ -38,23 +38,24 @@ setup(
                 "src/parser.c",
                 # NOTE: if your language uses an external scanner, add it here.
             ],
-            extra_compile_args=[
-                "-std=c11",
-            ] if system() != "Windows" else [
-                "/std:c11",
-                "/utf-8",
-            ],
+            extra_compile_args=(
+                [
+                    "-std=c11",
+                ]
+                if system() != "Windows"
+                else [
+                    "/std:c11",
+                    "/utf-8",
+                ]
+            ),
             define_macros=[
-                ("Py_LIMITED_API", "0x03080000"),
-                ("PY_SSIZE_T_CLEAN", None)
+                ("Py_LIMITED_API", "0x03090000"),
+                ("PY_SSIZE_T_CLEAN", None),
             ],
             include_dirs=["src"],
             py_limited_api=True,
         )
     ],
-    cmdclass={
-        "build": Build,
-        "bdist_wheel": BdistWheel
-    },
-    zip_safe=False
+    cmdclass={"build": Build, "bdist_wheel": BdistWheel},
+    zip_safe=False,
 )
