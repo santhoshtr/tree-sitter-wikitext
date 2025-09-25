42c571f286ecc6f489416390eb1047e3ee6c4aa8
Santhosh Thottingal <santhosh.thottingal@gmail.com>
2025-09-25 11:45:03 +0530

    Fix go tests

--- bindings/go/binding_test.go
+++ bindings/go/binding_test.go
@@ -3,8 +3,8 @@ package tree_sitter_wikitext_test
 import (
 	"testing"
 
-	tree_sitter "github.com/smacker/go-tree-sitter"
-	"github.com/tree-sitter/tree-sitter-wikitext"
+	tree_sitter "github.com/tree-sitter/go-tree-sitter"
+	tree_sitter_wikitext "github.com/santhoshtr/tree-sitter-wikitext/bindings/go"
 )
 
 func TestCanLoadGrammar(t *testing.T) {
